ced3d23911120f82a9683a5648ab54b105976b8c
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-09-11 09:44:32 +0200

    service parametrization: config and secrets

--- contexts/course/src/main/kotlin/com/codely/course/infrastructure/persistence/InMemoryCourseRepository.kt
+++ contexts/course/src/main/kotlin/com/codely/course/infrastructure/persistence/InMemoryCourseRepository.kt
@@ -2,9 +2,15 @@ package com.codely.course.infrastructure.persistence
 
 import com.codely.course.domain.course.Course
 import com.codely.course.domain.course.CourseRepository
-class InMemoryCourseRepository() : CourseRepository {
 
+class DatabaseConnectionData(var username: String = "", var password: String = "")
+class InMemoryCourseRepository(private val connectionData: DatabaseConnectionData) : CourseRepository {
     val repository: MutableList<Course> = mutableListOf<Course>()
+
+    init {
+        println()
+    }
+
     override fun save(course: Course) {
         repository.add(course)
     }
