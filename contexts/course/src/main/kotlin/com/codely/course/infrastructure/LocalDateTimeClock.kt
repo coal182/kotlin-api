b232f3e055286677468efb76a68b7dab7c2aff7a
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-09-11 14:28:51 +0200

    postgres as repository

--- contexts/course/src/main/kotlin/com/codely/course/infrastructure/LocalDateTimeClock.kt
+++ contexts/course/src/main/kotlin/com/codely/course/infrastructure/LocalDateTimeClock.kt
@@ -3,6 +3,6 @@ package com.codely.course.infrastructure
 import com.codely.course.domain.Clock
 import java.time.LocalDateTime
 
-class LocalDateTimeClock: Clock {
+class LocalDateTimeClock : Clock {
     override fun now(): LocalDateTime = LocalDateTime.now()
 }
--- src/main/kotlin/com/codely/config/DatabaseConfig.kt
+++ src/main/kotlin/com/codely/config/DatabaseConfig.kt
@@ -1,10 +1,11 @@
 package com.codely.config
 
 import com.codely.course.infrastructure.persistence.DatabaseConnectionData
-import com.codely.course.infrastructure.persistence.InMemoryCourseRepository
+import com.codely.course.infrastructure.persistence.PostgreCourseRepository
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
 @Configuration
 class DatabaseConfig {
@@ -14,5 +15,5 @@ class DatabaseConfig {
     fun databaseConnectionData() = DatabaseConnectionData()
 
     @Bean
-    fun courseRepository(databaseConnectionData: DatabaseConnectionData) = InMemoryCourseRepository(databaseConnectionData)
+    fun courseRepository(jdbcTemplate: NamedParameterJdbcTemplate) = PostgreCourseRepository(jdbcTemplate)
 }
